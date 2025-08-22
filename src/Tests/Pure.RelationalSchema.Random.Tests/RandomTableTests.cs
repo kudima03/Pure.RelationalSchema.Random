9caac03144ef9c32cd59e8e430f850280fdf580e
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-23 00:13:17 +0300

    Added RandomTableTests

---
+++ src/Tests/Pure.RelationalSchema.Random.Tests/RandomTableTests.cs
@@ -0,0 +1,94 @@
+using Pure.HashCodes;
+using Pure.RelationalSchema.Abstractions.Table;
+using Pure.RelationalSchema.HashCodes;
+
+namespace Pure.RelationalSchema.Random.Tests;
+
+public sealed record RandomTableTests
+{
+    [Fact]
+    public void InitializeNameAsCached()
+    {
+        ITable table = new RandomTable();
+
+        Assert.Equal(
+            new DeterminedHash(table.Name),
+            new DeterminedHash(table.Name),
+            new DeterminedHashEqualityComparer()
+        );
+    }
+
+    [Fact]
+    public void InitializeColumnsAsCached()
+    {
+        ITable table = new RandomTable();
+
+        Assert.Equal(
+            new AggregatedHash(table.Columns.Select(x => new ColumnHash(x))),
+            new AggregatedHash(table.Columns.Select(x => new ColumnHash(x))),
+            new DeterminedHashEqualityComparer()
+        );
+    }
+
+    [Fact]
+    public void InitializeIndexesAsCached()
+    {
+        ITable table = new RandomTable();
+
+        Assert.Equal(
+            new AggregatedHash(table.Indexes.Select(x => new IndexHash(x))),
+            new AggregatedHash(table.Indexes.Select(x => new IndexHash(x))),
+            new DeterminedHashEqualityComparer()
+        );
+    }
+
+    [Fact]
+    public void ProduceRandomValuesWithSharedProvider()
+    {
+        const int count = 100;
+
+        System.Random random = new System.Random();
+
+        IEnumerable<ITable> randoms = Enumerable
+            .Range(0, count)
+            .Select(_ => new RandomTable(random));
+
+        Assert.Equal(
+            count,
+            randoms
+                .Select(x => new TableHash(x))
+                .Distinct(new DeterminedHashEqualityComparer())
+                .Count()
+        );
+    }
+
+    [Fact]
+    public void ProduceRandomValues()
+    {
+        const int count = 100;
+
+        IEnumerable<ITable> randoms = Enumerable
+            .Range(0, count)
+            .Select(_ => new RandomTable());
+
+        Assert.Equal(
+            count,
+            randoms
+                .Select(x => new TableHash(x))
+                .Distinct(new DeterminedHashEqualityComparer())
+                .Count()
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() => new RandomTable().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() => new RandomTable().ToString());
+    }
+}
