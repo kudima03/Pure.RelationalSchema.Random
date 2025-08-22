e0110a291c84bf637a2cd1ea917383a7d40ee7de
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-22 16:37:23 +0300

    Added RandomColumnType

---
+++ src/Pure.RelationalSchema.Random/RandomColumnType.cs
@@ -0,0 +1,33 @@
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.Cached.String;
+using Pure.Primitives.Random.Number;
+using Pure.Primitives.Random.String;
+using Pure.RelationalSchema.Abstractions.ColumnType;
+
+namespace Pure.RelationalSchema.Random;
+
+public sealed record RandomColumnType : IColumnType
+{
+    public RandomColumnType()
+        : this(new System.Random()) { }
+
+    public RandomColumnType(System.Random random)
+        : this(new CachedString(new RandomString(new RandomUShort(random)))) { }
+
+    private RandomColumnType(IString name)
+    {
+        Name = name;
+    }
+
+    public IString Name { get; }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
