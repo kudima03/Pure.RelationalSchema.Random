ef31b105fd19a84e28fddf90c95b706bb0b5022e
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-22 16:37:10 +0300

    Added DeterminedHashEqualityComparer

---
+++ src/Tests/Pure.RelationalSchema.Random.Tests/DeterminedHashEqualityComparer.cs
@@ -0,0 +1,18 @@
+using Pure.HashCodes;
+
+namespace Pure.RelationalSchema.Random.Tests;
+
+internal sealed record DeterminedHashEqualityComparer : IEqualityComparer<IDeterminedHash>
+{
+    public bool Equals(IDeterminedHash? x, IDeterminedHash? y)
+    {
+        return x!.SequenceEqual(y!);
+    }
+
+    public int GetHashCode(IDeterminedHash obj)
+    {
+        HashCode hash = new HashCode();
+        hash.AddBytes(obj.ToArray());
+        return hash.ToHashCode();
+    }
+}
