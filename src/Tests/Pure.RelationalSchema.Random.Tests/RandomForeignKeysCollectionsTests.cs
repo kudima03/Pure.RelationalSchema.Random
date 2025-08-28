5a1d6614022896275ddf928907b2d8b5feb50462
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-28 22:37:23 +0300

    Implemented tests for RandomForeignKeys

---
+++ src/Tests/Pure.RelationalSchema.Random.Tests/RandomForeignKeysCollectionsTests.cs
@@ -0,0 +1,290 @@
+using System.Collections;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.String;
+using Pure.RelationalSchema.Abstractions.ForeignKey;
+using Pure.RelationalSchema.HashCodes;
+
+namespace Pure.RelationalSchema.Random.Tests;
+
+using Random = System.Random;
+
+public sealed record RandomForeignKeysCollectionsTests
+{
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        IEnumerable randoms = new RandomForeignKeysCollection(new UShort(10));
+
+        ICollection<IForeignKey> casted = [];
+
+        foreach (object item in randoms)
+        {
+            IForeignKey castedItem = (IForeignKey)item;
+            casted.Add(castedItem);
+        }
+
+        Assert.NotEmpty(casted);
+    }
+
+    [Fact]
+    public void ProduceRandomValuesWithSharedProvider()
+    {
+        const int count = 5;
+
+        Random random = new Random();
+
+        RandomTablesCollection randomReferencingTables = new RandomTablesCollection(
+            new UShort(count),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomColumnsCollection(
+                    new UShort(count),
+                    new RandomStringCollection(
+                        new UShort(count),
+                        new UShort(count),
+                        random
+                    ),
+                    new RandomColumnTypesCollection(
+                        new UShort(count),
+                        new RandomStringCollection(
+                            new UShort(count),
+                            new UShort(count),
+                            random
+                        )
+                    )
+                )),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomIndexesCollection(
+                    new UShort(count),
+                    Enumerable
+                        .Range(0, count)
+                        .Select(_ => new RandomColumnsCollection(
+                            new UShort(count),
+                            new RandomStringCollection(
+                                new UShort(count),
+                                new UShort(count),
+                                random
+                            ),
+                            new RandomColumnTypesCollection(
+                                new UShort(count),
+                                new RandomStringCollection(
+                                    new UShort(count),
+                                    new UShort(count),
+                                    random
+                                )
+                            )
+                        )),
+                    random
+                )),
+            random
+        );
+
+        RandomTablesCollection randomReferencedTables = new RandomTablesCollection(
+            new UShort(count),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomColumnsCollection(
+                    new UShort(count),
+                    new RandomStringCollection(
+                        new UShort(count),
+                        new UShort(count),
+                        random
+                    ),
+                    new RandomColumnTypesCollection(
+                        new UShort(count),
+                        new RandomStringCollection(
+                            new UShort(count),
+                            new UShort(count),
+                            random
+                        )
+                    )
+                )),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomIndexesCollection(
+                    new UShort(count),
+                    Enumerable
+                        .Range(0, count)
+                        .Select(_ => new RandomColumnsCollection(
+                            new UShort(count),
+                            new RandomStringCollection(
+                                new UShort(count),
+                                new UShort(count),
+                                random
+                            ),
+                            new RandomColumnTypesCollection(
+                                new UShort(count),
+                                new RandomStringCollection(
+                                    new UShort(count),
+                                    new UShort(count),
+                                    random
+                                )
+                            )
+                        )),
+                    random
+                )),
+            random
+        );
+
+        IEnumerable<IForeignKey> foreignKeys = new RandomForeignKeysCollection(
+            new UShort(count),
+            randomReferencingTables,
+            new RandomColumnsCollection(
+                new UShort(count),
+                new RandomStringCollection(new UShort(count), new UShort(count), random),
+                new RandomColumnTypesCollection(
+                    new UShort(count),
+                    new RandomStringCollection(
+                        new UShort(count),
+                        new UShort(count),
+                        random
+                    )
+                )
+            ),
+            randomReferencedTables,
+            new RandomColumnsCollection(
+                new UShort(count),
+                new RandomStringCollection(new UShort(count), new UShort(count), random),
+                new RandomColumnTypesCollection(
+                    new UShort(count),
+                    new RandomStringCollection(
+                        new UShort(count),
+                        new UShort(count),
+                        random
+                    )
+                )
+            )
+        );
+
+        Assert.Equal(
+            count,
+            foreignKeys
+                .Select(x => new ForeignKeyHash(x))
+                .Distinct(new DeterminedHashEqualityComparer())
+                .Count()
+        );
+    }
+
+    [Fact]
+    public void ProduceRandomValues()
+    {
+        const int count = 5;
+
+        RandomTablesCollection randomReferencingTables = new RandomTablesCollection(
+            new UShort(count),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomColumnsCollection(
+                    new UShort(count),
+                    new RandomStringCollection(new UShort(count), new UShort(count)),
+                    new RandomColumnTypesCollection(
+                        new UShort(count),
+                        new RandomStringCollection(new UShort(count), new UShort(count))
+                    )
+                )),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomIndexesCollection(
+                    new UShort(count),
+                    Enumerable
+                        .Range(0, count)
+                        .Select(_ => new RandomColumnsCollection(
+                            new UShort(count),
+                            new RandomStringCollection(
+                                new UShort(count),
+                                new UShort(count)
+                            ),
+                            new RandomColumnTypesCollection(
+                                new UShort(count),
+                                new RandomStringCollection(
+                                    new UShort(count),
+                                    new UShort(count)
+                                )
+                            )
+                        ))
+                ))
+        );
+
+        RandomTablesCollection randomReferencedTables = new RandomTablesCollection(
+            new UShort(count),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomColumnsCollection(
+                    new UShort(count),
+                    new RandomStringCollection(new UShort(count), new UShort(count)),
+                    new RandomColumnTypesCollection(
+                        new UShort(count),
+                        new RandomStringCollection(new UShort(count), new UShort(count))
+                    )
+                )),
+            Enumerable
+                .Range(0, count)
+                .Select(_ => new RandomIndexesCollection(
+                    new UShort(count),
+                    Enumerable
+                        .Range(0, count)
+                        .Select(_ => new RandomColumnsCollection(
+                            new UShort(count),
+                            new RandomStringCollection(
+                                new UShort(count),
+                                new UShort(count)
+                            ),
+                            new RandomColumnTypesCollection(
+                                new UShort(count),
+                                new RandomStringCollection(
+                                    new UShort(count),
+                                    new UShort(count)
+                                )
+                            )
+                        ))
+                ))
+        );
+
+        IEnumerable<IForeignKey> foreignKeys = new RandomForeignKeysCollection(
+            new UShort(count),
+            randomReferencingTables,
+            new RandomColumnsCollection(
+                new UShort(count),
+                new RandomStringCollection(new UShort(count), new UShort(count)),
+                new RandomColumnTypesCollection(
+                    new UShort(count),
+                    new RandomStringCollection(new UShort(count), new UShort(count))
+                )
+            ),
+            randomReferencedTables,
+            new RandomColumnsCollection(
+                new UShort(count),
+                new RandomStringCollection(new UShort(count), new UShort(count)),
+                new RandomColumnTypesCollection(
+                    new UShort(count),
+                    new RandomStringCollection(new UShort(count), new UShort(count))
+                )
+            )
+        );
+
+        Assert.Equal(
+            count,
+            foreignKeys
+                .Select(x => new ForeignKeyHash(x))
+                .Distinct(new DeterminedHashEqualityComparer())
+                .Count()
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new RandomForeignKeysCollection().GetHashCode()
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new RandomForeignKeysCollection().ToString()
+        );
+    }
+}
